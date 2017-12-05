c4fc5ec404be7c7b4f451a1f59a0e249fdaa32fd
Juan Amari <juan@amari.com.ar>
2017-12-05 20:49:58 -0300

    3.25

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,20 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(left, right) => 1 + size(left) + size(right)
+  }
 
+}
 
+object testTree {
 
+  def main(args: Array[String]): Unit = {
+
+    println("\nsize")
+    println(Tree.size(Branch(Leaf(1), Leaf(2))))
+    println(Tree.size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
+    println(Tree.size(Leaf(1)))
+  }
 }
