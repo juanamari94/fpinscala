663bf6bdb8d559d62f724a312b1bcb7bdfdbff9a
Juan Amari <juan.amari@redb.ee>
2017-12-06 15:15:36 -0300

    3.27

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -12,9 +12,14 @@ object Tree {
     case Branch(left, right) => 1 + size(left) + size(right)
   }
 
-  def max(tree: Tree[Int]): Int = tree match {
+  def maximum(tree: Tree[Int]): Int = tree match {
     case Leaf(a) => a
-    case Branch(left, right) => max(left).max(max(right))
+    case Branch(left, right) => maximum(left).max(maximum(right))
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 0
+    case Branch(left, right) => 1 + depth(left).max(depth(right))
   }
 }
 
@@ -28,9 +33,14 @@ object testTree {
     println(Tree.size(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
 
     println("\nmax")
-    println(Tree.max(Leaf(1)))
-    println(Tree.max(Branch(Leaf(1), Leaf(2))))
-    println(Tree.max(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
-    println(Tree.max(Branch(Branch(Leaf(3), Leaf(4)), Branch(Leaf(1), Leaf(2)))))
+    println(Tree.maximum(Leaf(1)))
+    println(Tree.maximum(Branch(Leaf(1), Leaf(2))))
+    println(Tree.maximum(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
+    println(Tree.maximum(Branch(Branch(Leaf(3), Leaf(4)), Branch(Leaf(1), Leaf(2)))))
+
+    println("\ndepth")
+    println(Tree.depth(Leaf(1)))
+    println(Tree.depth(Branch(Leaf(1), Leaf(2))))
+    println(Tree.depth(Branch(Branch(Leaf(3), Leaf(4)), Leaf(2))))
   }
 }
