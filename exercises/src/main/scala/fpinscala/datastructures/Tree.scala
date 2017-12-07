d4ae85afc6ff72dc4fc4e1c48531ce4c23dd1d32
Juan Amari <juan.amari@redb.ee>
2017-12-07 10:59:30 -0300

    added 3.29

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -26,6 +26,19 @@ object Tree {
     case Leaf(a) => Leaf(f(a))
     case Branch(left, right) => Branch(map(left)(f), map(right)(f))
   }
+
+  def fold[A,B](t: Tree[A])(l: A => B)(b: (B,B) => B): B = t match {
+    case Leaf(a) => l(a)
+    case Branch(left, right) => b(fold(left)(l)(b), fold(right)(l)(b))
+  }
+
+  def foldedSize[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+  def foldedMaximum(t: Tree[Int]): Int = fold(t)(a => a)(_ max _)
+
+  def foldedDepth[A](t: Tree[A]): Int = fold(t)(_ => 0)(1 + _ max _)
+
+  def foldedMap[A, B](t: Tree[A])(f: A => B): Tree[B] = fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 }
 
 object testTree {
@@ -50,5 +63,25 @@ object testTree {
 
     println("\nmap")
     println(Tree.map(Branch(Leaf(1), Leaf(2)))(value => value + 1))
+
+    println("\nfoldedSize")
+    println(Tree.foldedSize(Leaf(1)))
+    println(Tree.foldedSize(Branch(Leaf(1), Leaf(2))))
+    println(Tree.foldedSize(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
+
+    println("\nfoldedMaximum")
+    println(Tree.foldedMaximum(Leaf(1)))
+    println(Tree.foldedMaximum(Branch(Leaf(1), Leaf(2))))
+    println(Tree.foldedMaximum(Branch(Branch(Leaf(1), Leaf(2)), Branch(Leaf(3), Leaf(4)))))
+    println(Tree.foldedMaximum(Branch(Branch(Leaf(3), Leaf(4)), Branch(Leaf(1), Leaf(2)))))
+
+    println("\nfoldedDepth")
+    println(Tree.foldedDepth(Leaf(1)))
+    println(Tree.foldedDepth(Branch(Leaf(1), Leaf(2))))
+    println(Tree.foldedDepth(Branch(Branch(Leaf(3), Leaf(4)), Leaf(2))))
+
+    println("\nfoldedMap")
+    println(Tree.foldedMap(Branch(Leaf(1), Leaf(2)))(value => value + 1))
+
   }
 }
